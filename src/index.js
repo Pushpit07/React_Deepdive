cee31caa94bab49d0c96df0243e0f038b6716ffb
Pushpit <pushpit07@gmail.com>
2021-07-12 20:42:24 +0530

    Book style
    Signed-off-by: Pushpit <pushpit07@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,39 +1,27 @@
 import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
+// CSS
+import "./index.css";
 
-// stateless functional component
-// always return jsx
 function BookList() {
 	return (
-		<section>
-			<Book />
-			<Book />
-			<Book />
+		<section className="booklist">
 			<Book />
 		</section>
 	);
 }
 
 const Book = () => {
+	const title = "Think and Grow Rich";
+	const author = "Napoleon Hill";
+
 	return (
-		<article>
-			<Image />
-			<Title />
-			<Author />
+		<article className="book">
+			<img src="https://m.media-amazon.com/images/I/711RcAn0EiL._AC_UY436_FMwebp_QL65_.jpg" alt="" />;
+			<h1>{title}</h1>
+			<h4>{author}</h4>
 		</article>
 	);
-};
-
-const Image = () => {
-	return <img src="https://m.media-amazon.com/images/I/711RcAn0EiL._AC_UY436_FMwebp_QL65_.jpg" alt="" />;
-};
-
-const Title = () => {
-	return <h1>Think and Grow Rich</h1>;
-};
-
-const Author = () => {
-	return <h4>Napoleon Hill</h4>;
 };
 
 ReactDOM.render(<BookList />, document.getElementById("root"));
